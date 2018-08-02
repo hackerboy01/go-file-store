8429aec90241df993c7d91c61689e9b6df221812
Mickel Jiang <mickel.jiang@gllue.com>
2018-08-02 09:26:51 +0800

    :custard:  配置数据库连接

--- models/init.go
+++ models/init.go
@@ -1,5 +1,22 @@
 package models
 
-func init() {
+import (
+	"github.com/astaxie/beego"
+	"fmt"
+	"strings"
+	"github.com/astaxie/beego/orm"
+)
 
+// 初始化数据库连接
+func init() {
+	dbHost := beego.AppConfig.String("dbHost")
+	dbPort := beego.AppConfig.String("dbPort")
+	dbUser := beego.AppConfig.String("dbUser")
+	dbPassword := beego.AppConfig.String("dbPassword")
+	dbDatabase := beego.AppConfig.String("dbDatabase")
+
+	dbSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPassword, dbHost, dbPort, dbDatabase)
+	dbSource = strings.Replace(dbSource, "Local", "Asia%2FShanghai", 1)
+	orm.RegisterDataBase("default", "mysql", dbSource, 30, 30)
 }
